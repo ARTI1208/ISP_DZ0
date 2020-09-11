69c618412fdc9d3256c31cb4800a11b42c70ba90
ARTI1208 <leonardo906@mail.ru>
2020-09-11 21:51:59 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ISP_DZ1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ISP_DZ1.dir/main.cpp.o"
+  "ISP_DZ1"
+  "ISP_DZ1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ISP_DZ1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
